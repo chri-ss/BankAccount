28b498fcd688d7f15bf7b757c86f160a9fa95667
chris <christorper@gmail.com>
2025-04-04 11:06:47 -0700

    SavingsAccount parameterized constructor

--- SavingsAccount.h
+++ SavingsAccount.h
@@ -2,6 +2,28 @@
 #define SAVINGSACCOUNT_H
 #include "BankAccount.h"
 class SavingsAccount : public BankAccount {
+public:
+  SavingsAccount(double intRate, double wdFee, int freeWdLimit, int availFreeWd)
+      : BankAccount() {
+    if (intRate <= 0) {
+      throw NegativeAmountException("Error, negative interest rate");
+    }
+    if (wdFee <= 0) {
+      throw NegativeAmountException("Error, negative withdrawal fee");
+    }
+    if (freeWdLimit <= 0) {
+      throw NegativeAmountException("Error, negative withdrawal limit");
+    }
+    if (availFreeWd <= 0) {
+      throw NegativeAmountException("Error, negative available withdrawal");
+    }
+
+    interestRate = intRate;
+    withdrawFee = wdFee;
+    freeWithdrawlimit = freeWdLimit;
+    availableFreeWithdrawal = availFreeWd;
+  }
+
 private:
   double interestRate;
   double withdrawFee;
