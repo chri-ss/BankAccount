7b82d6d839b64099a78d1d7c0b18928438ceb39a
Chris <christorper@gmail.com>
2025-04-06 14:57:10 -0700

    merge header and cpp files

--- BankAccount.h
+++ BankAccount.h
@@ -42,4 +42,33 @@ private:
   std::string accountHolder;
   int accountNumber;
 };
+
+BankAccount::BankAccount()
+    : accountHolder(""), accountNumber(0), balance(0.0) {}
+
+BankAccount::BankAccount(std::string acctHldr, int acctNum, double bal)
+    : accountHolder(acctHldr), accountNumber(acctNum), balance(bal) {}
+
+BankAccount::~BankAccount() {}
+
+BankAccount::InsufficientFundsException::InsufficientFundsException(
+    std::string error) {
+  errMsg = error;
+}
+BankAccount::NegativeAmountException::NegativeAmountException(
+    std::string error) {
+  errMsg = error;
+}
+
+const char *BankAccount ::InsufficientFundsException::what() {
+  return errMsg.c_str();
+}
+
+const char *BankAccount::NegativeAmountException::what() {
+  return errMsg.c_str();
+}
+
+void BankAccount::deposit(double) {}
+void BankAccount::display() noexcept {}
+
 #endif
--- SavingsAccount.h
+++ SavingsAccount.h
@@ -28,4 +28,67 @@ private:
   int freeWithdrawlimit;
   int availableFreeWithdrawal;
 };
+
+SavingsAccount::SavingsAccount(std::string acctHldr, int acctNum, double bal,
+                               double intRate, double wdFee, int freeWdLimit,
+                               int availFreeWd)
+    : BankAccount(acctHldr, acctNum, bal) {
+  if (bal <= 0) {
+    throw NegativeAmountException("Error, balance must be positive");
+  }
+  if (intRate <= 0) {
+    throw NegativeAmountException("Error, negative interest rate");
+  }
+  if (wdFee <= 0) {
+    throw NegativeAmountException("Error, negative withdrawal fee");
+  }
+  if (freeWdLimit <= 0) {
+    throw NegativeAmountException("Error, negative withdrawal limit");
+  }
+  if (availFreeWd <= 0) {
+    throw NegativeAmountException("Error, negative available withdrawal");
+  }
+
+  interestRate = intRate;
+  withdrawFee = wdFee;
+  freeWithdrawlimit = freeWdLimit;
+  availableFreeWithdrawal = availFreeWd;
+}
+
+double SavingsAccount::withdraw(double amount) {
+  balance -= amount;
+  if (balance < 0) {
+    balance += amount;
+    throw InsufficientFundsException(
+        "Error, insufficient funds for withdrawal");
+  }
+
+  if (freeWithdrawlimit >= availableFreeWithdrawal) {
+    std::cout << "You have exceeded your available free " << freeWithdrawlimit
+              << " withdrawals for the month. A fee of " << withdrawFee
+              << " will be applied to your account." << std::endl;
+    balance -= withdrawFee;
+  }
+  return amount;
+}
+
+void SavingsAccount::deposit(double amount) {
+  if (amount < 0) {
+    throw NegativeAmountException("Error, can't deposit a negative amount");
+  }
+  balance += amount;
+}
+
+void SavingsAccount::display() noexcept {
+  std::cout << "Savings Account: " << getAccountNumber() << std::endl;
+  std::cout << "Account Holder: " << getAccountHolder() << std::endl;
+  std::cout << std::fixed << std::setprecision(2) << "Balance: " << getBalance()
+            << std::endl;
+}
+
+void SavingsAccount::applyInterest() { balance += balance * interestRate; }
+
+void SavingsAccount::resetMonthlyWithdrawLimit() {
+  availableFreeWithdrawal = freeWithdrawlimit;
+}
 #endif
